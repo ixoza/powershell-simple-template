525306f8e9d1b7b89bbbd4e8c0be653bd30f4189
Artem <58910248+ixoza@users.noreply.github.com>
2024-04-17 11:18:50 +0200

    add basic things

--- template-pwsh.ps1
+++ template-pwsh.ps1
@@ -1,1 +1,14 @@
-placeholder
+#   ---------------------------
+#   Made by : 
+#   Start dete : 
+#   Version : 
+#   ---------------------------
+
+#   Vars
+$infoLog = "[INFO] $(Get-Date -Format 'HH:mm:ss')"
+$errorLog = "[ERROR] $(Get-Date -Format 'HH:mm:ss')"
+$warnLog = "[WARN] $(Get-Date -Format 'HH:mm:ss')"
+
+Write-Output "$infoLog Running from $printDir"
+Write-Output "$infoLog It's $time"
+Write-Output "$infoLog My pwsh version is $($PSVersionTable.PSVersion)"
