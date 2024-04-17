251d2d803819958ceac097be0195ccbdb19c27d9
Artem <58910248+ixoza@users.noreply.github.com>
2024-04-17 14:47:29 +0200

    removed unnecessary code

--- template-pwsh.ps1
+++ template-pwsh.ps1
@@ -4,11 +4,13 @@
 #   Version : 
 #   ---------------------------
 
+#>
+
 #   Vars
 $infoLog = "[INFO] $(Get-Date -Format 'HH:mm:ss')"
 $errorLog = "[ERROR] $(Get-Date -Format 'HH:mm:ss')"
 $warnLog = "[WARN] $(Get-Date -Format 'HH:mm:ss')"
 
-Write-Output "$infoLog Running from $printDir"
-Write-Output "$infoLog It's $time"
-Write-Output "$infoLog My pwsh version is $($PSVersionTable.PSVersion)"
+Write-Output "$infoLog Running from: $(Get-Location)"
+Write-Output "$infoLog It's: $(Get-Date -Format "yyyy-MM-dd HH:mm:ss")"
+Write-Output "$infoLog My pwsh version is: $($PSVersionTable.PSVersion)"
