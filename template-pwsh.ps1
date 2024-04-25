69ff6964e3eb15a0300afe665b515605ef4f4e73
Artem <58910248+ixoza@users.noreply.github.com>
2024-04-25 10:38:32 +0200

    i'm dumb

--- template-pwsh.ps1
+++ template-pwsh.ps1
@@ -2,14 +2,22 @@
 #   Made by : 
 #   Start dete : 
 #   Version : 
-#   Description :
+#   Description : 
 #   ---------------------------
 
 #   Vars
-$infoLog = "[INFO] $(Get-Date -Format 'HH:mm:ss')"
-$errorLog = "[ERROR] $(Get-Date -Format 'HH:mm:ss')"
-$warnLog = "[WARN] $(Get-Date -Format 'HH:mm:ss')"
+function logInfo {
+    $date = "[LOG] [INFO] $(Get-Date -Format 'HH:mm:ss')"
+    return $date
+}
+function logError {
+    $date = "[LOG] [ERROR] $(Get-Date -Format 'HH:mm:ss')"
+    return $date
+}
+function logWarn {
+    $date = "[LOG] [WARN] $(Get-Date -Format 'HH:mm:ss')"
+    return $date
+}
 
-Write-Output "$infoLog Running from: $(Get-Location)"
-Write-Output "$infoLog It's: $(Get-Date -Format "yyyy-MM-dd HH:mm:ss")"
-Write-Output "$infoLog My pwsh version is: $($PSVersionTable.PSVersion)"
+Write-Output "$(logInfo) Running from: $(Get-Location)"
+Write-Output "$(logInfo) My pwsh version is: $($PSVersionTable.PSVersion)"
