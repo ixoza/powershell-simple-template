20285b6bf9ea84e45dddcd08d20859ed57bcda15
Artem <58910248+ixoza@users.noreply.github.com>
2024-04-26 23:40:00 +0200

    added some comments and pwsh installation command

--- template-pwsh.ps1
+++ template-pwsh.ps1
@@ -1,3 +1,6 @@
+#            installs latest version of powershell
+#    iex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI"
+
 #   ---------------------------
 #   Made by : 
 #   Start dete : 
@@ -6,6 +9,7 @@
 #   ---------------------------
 
 #   Vars
+
 function logInfo {
     $date = "[LOG] [INFO] $(Get-Date -Format 'HH:mm:ss')"
     return $date
@@ -19,5 +23,11 @@ function logWarn {
     return $date
 }
 
+#    Use redirect like if you want to log in a file
+#    Define a log file in current directory
+#    $logFile = ./log.txt
+#    Write-Output "*some log*" >> $logFile
+
 Write-Output "$(logInfo) Running from: $(Get-Location)"
 Write-Output "$(logInfo) My pwsh version is: $($PSVersionTable.PSVersion)"
+Write-Output "$(logInfo) My hostname: $(hostname)"
