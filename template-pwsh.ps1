31ebad126cce3c6ad8f67e8251978debd044c69f
Artem <58910248+ixoza@users.noreply.github.com>
2024-10-16 13:08:19 +0200

    Update template-pwsh.ps1

--- template-pwsh.ps1
+++ template-pwsh.ps1
@@ -1,6 +1,6 @@
-#            installs latest version of powershell
+#                    /!\    Careful with AVs    /!\
+#                installs latest version of powershell
 #    iex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI"
-
 #   ---------------------------
 #   Made by : 
 #   Start dete : 
